237400c3454d8ee2577ab66b5b775d101f2918f4
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2024-10-05 17:00:38 +0530

    run experiment for sample & get avg

--- Lab-1/main.c
+++ Lab-1/main.c
@@ -4,12 +4,14 @@
 #include <pthread.h>
 #include <time.h>
 
-#define N 1000  // Initial size of the linked list
-#define M 10000 // Number of operations
-#define THREAD_COUNT 4 // Number of threads to run concurrently
+#define N 1000         // Initial size of the linked list
+#define M 10000         // Number of operations
+#define THREAD_COUNT 1 // Number of threads to run concurrently
+#define SAMPLE_SIZE 385  // Number of times each experiment is run
 
 // Function for timing
-double measure_time(void (*func)(void)) {
+double measure_time(void (*func)(void))
+{
     clock_t start = clock();
     func();
     clock_t end = clock();
@@ -17,17 +19,24 @@ double measure_time(void (*func)(void)) {
 }
 
 // Example thread work function for mutex and read-write lock
-void* thread_work(void* arg) {
+void *thread_work(void *arg)
+{
     int ops = M / THREAD_COUNT;
-    for (int i = 0; i < ops; i++) {
+    for (int i = 0; i < ops; i++)
+    {
         int op_type = rand() % 3;
         int value = rand() % 65536; // Value between 0 and 2^16-1
 
-        if (op_type == 0) {
+        if (op_type == 0)
+        {
             member_mutex(value); // Perform member operation
-        } else if (op_type == 1) {
+        }
+        else if (op_type == 1)
+        {
             insert_mutex(value); // Perform insert operation
-        } else {
+        }
+        else
+        {
             delete_mutex(value); // Perform delete operation
         }
     }
@@ -35,63 +44,122 @@ void* thread_work(void* arg) {
 }
 
 // Function to run linked list operations with mutex
-void run_mutex_operations() {
+double run_mutex_operations()
+{
     init_mutex(); // Initialize mutex linked list
     pthread_t threads[THREAD_COUNT];
 
-    for (int i = 0; i < THREAD_COUNT; i++) {
+    clock_t start = clock();
+
+    for (int i = 0; i < THREAD_COUNT; i++)
+    {
         pthread_create(&threads[i], NULL, thread_work, NULL);
     }
 
-    for (int i = 0; i < THREAD_COUNT; i++) {
+    for (int i = 0; i < THREAD_COUNT; i++)
+    {
         pthread_join(threads[i], NULL);
     }
+
+    clock_t end = clock();
+    return ((double)(end - start)) / CLOCKS_PER_SEC;
 }
 
 // Function to run linked list operations with read-write locks
-void run_rwlock_operations() {
+double run_rwlock_operations()
+{
     init_rwlock(); // Initialize read-write lock linked list
     pthread_t threads[THREAD_COUNT];
 
-    for (int i = 0; i < THREAD_COUNT; i++) {
+    clock_t start = clock();
+
+    for (int i = 0; i < THREAD_COUNT; i++)
+    {
         pthread_create(&threads[i], NULL, thread_work, NULL);
     }
 
-    for (int i = 0; i < THREAD_COUNT; i++) {
+    for (int i = 0; i < THREAD_COUNT; i++)
+    {
         pthread_join(threads[i], NULL);
     }
+
+    clock_t end = clock();
+    return ((double)(end - start)) / CLOCKS_PER_SEC;
 }
 
 // Function to run linked list operations serially
-void run_serial_operations() {
-    for (int i = 0; i < M; i++) {
+double run_serial_operations()
+{
+    clock_t start = clock();
+
+    for (int i = 0; i < M; i++)
+    {
         int op_type = rand() % 3;
         int value = rand() % 65536; // Value between 0 and 2^16-1
 
-        if (op_type == 0) {
+        if (op_type == 0)
+        {
             member_serial(value); // Perform member operation
-        } else if (op_type == 1) {
+        }
+        else if (op_type == 1)
+        {
             insert_serial(value); // Perform insert operation
-        } else {
+        }
+        else
+        {
             delete_serial(value); // Perform delete operation
         }
     }
+    clock_t end = clock();
+    return ((double)(end - start)) / CLOCKS_PER_SEC;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     srand(time(NULL));
 
-    // Measure time for serial operations
-    double serial_time = measure_time(run_serial_operations);
-    printf("Time taken for Serial operations: %.6f seconds\n", serial_time);
+    double total_serial_time = 0.0;
+    double total_mutex_time = 0.0;
+    double total_rwlock_time = 0.0;
 
-    // Measure time for mutex operations
-    double mutex_time = measure_time(run_mutex_operations);
-    printf("Time taken for Mutex operations: %.6f seconds\n", mutex_time);
+    // Serial operations loop
+    printf("Running Serial operations...\n");
+    for (int i = 0; i < SAMPLE_SIZE; i++)
+    {
+        total_serial_time += run_serial_operations();
+        printf("\rProgress: %d/%d", i + 1, SAMPLE_SIZE);
+        fflush(stdout); // Force the output to be updated
+    }
+    printf("\n");
 
-    // Measure time for read-write lock operations
-    double rwlock_time = measure_time(run_rwlock_operations);
-    printf("Time taken for Read-Write Lock operations: %.6f seconds\n", rwlock_time);
+    // // Mutex operations loop
+    // printf("Running Mutex operations...\n");
+    // for (int i = 0; i < SAMPLE_SIZE; i++)
+    // {
+    //     total_mutex_time += run_mutex_operations();
+    //     printf("\rProgress: %d/%d", i + 1, SAMPLE_SIZE);
+    //     fflush(stdout); // Force the output to be updated
+    // }
+    // printf("\n");
+
+    // // Read-Write Lock operations loop
+    // printf("Running Read-Write Lock operations...\n");
+    // for (int i = 0; i < SAMPLE_SIZE; i++)
+    // {
+    //     total_rwlock_time += run_rwlock_operations();
+    //     printf("\rProgress: %d/%d", i + 1, SAMPLE_SIZE);
+    //     fflush(stdout); // Force the output to be updated
+    // }
+    // printf("\n");
+
+    // Calculate and print average times
+    double avg_serial_time = total_serial_time / SAMPLE_SIZE;
+    double avg_mutex_time = total_mutex_time / SAMPLE_SIZE;
+    double avg_rwlock_time = total_rwlock_time / SAMPLE_SIZE;
+
+    printf("Average time taken for Serial operations: %.6f seconds\n", avg_serial_time);
+    printf("Average time taken for Mutex operations: %.6f seconds\n", avg_mutex_time);
+    printf("Average time taken for Read-Write Lock operations: %.6f seconds\n", avg_rwlock_time);
 
     return 0;
 }
